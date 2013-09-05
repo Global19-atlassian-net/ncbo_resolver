507714589c172aed65bedf2bcad0d2e86335d16c
Paul R Alexander <palexander@stanford.edu>
2013-09-05 09:57:35 -0700

    Initialization

--- lib/ncbo_resolver.rb
+++ lib/ncbo_resolver.rb
@@ -1,4 +1,9 @@
 require "ncbo_resolver/version"
+require "ncbo_resolver/rest_helper"
+require "ncbo_resolver/population/classes"
+require "ncbo_resolver/population/ontologies"
+
+require 'zlib'
 
 module NCBO::Resolver
   # Your code goes here...
